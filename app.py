f13764f441ca3c5deb3b248cbcbbb27d84618941
mi418 <79618900+mi418@users.noreply.github.com>
2022-03-27 14:42:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
+import cv2
+import numpy as np
+from google.colab.patches import cv2_imshow
 
-def main():
-    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
-    st.write('Hello, World!')
+import pickle
 
+# from google.colab import drive
+# drive.mount('/content/drive')
 
-if __name__ == '__main__':
-    main()
+st.title("Hello World !")
+
+img = cv2.imread('/content/drive/MyDrive/あみぐるみ/original_png.png') # 画像の読み出し
+cv2_imshow(img)
+
+f = open('/content/drive/MyDrive/あみぐるみ/dmc色/dic_color.txt',"rb")
+dic_color = pickle.load(f)
