b2af1e82f9a2381260675d85554e8ac9da2498b0
mi418 <79618900+mi418@users.noreply.github.com>
2022-03-27 17:07:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pickle
 # st.title("Hello World !")
 
 img_col = cv2.imread('coler_png.png') # 画像の読み出し
-st.image(img_col, channels="RGB")
+st.image(img_col, channels="BGR")
 
 img = cv2.imread('original_png.png') # 画像の読み出し
 st.image(img)
